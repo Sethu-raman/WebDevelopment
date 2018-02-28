7a03e05c9a31c2c146194d4fbb6ea48d208c66d3
Sethuraman Solairajan <ssolairajan@seic.com>
2018-02-28 11:06:57 -0500

    added answer

--- interview.js
+++ interview.js
@@ -5,3 +5,12 @@ for (var i = 0; i < arr.length; i++) {
     console.log('Index: ' + i + ', element: ' + arr[i]);
   }, 3000);
 }
+
+// settimeout needs to be outside of the for loop to print the array elements
+/*
+setTimeout(function() {
+  for (var i = 0; i < arr.length; i++) {
+    console.log('Index: ' + i + ', element: ' + arr[i]);
+  };
+},3000)
+*/
